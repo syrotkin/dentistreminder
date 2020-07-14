267434bc4dcf4996a98962e21de7bc45a7e6ef23
syrotkin <syrotkin@gmail.com>
2020-07-14 10:19:33 +0200

    set font sizes: header - 12pt, cell - 14pt

--- DentistReminder/ReminderApplicationWinForms/MainForm.Designer.cs
+++ DentistReminder/ReminderApplicationWinForms/MainForm.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.patientGrid = new System.Windows.Forms.DataGridView();
             this.mainBindingSource = new System.Windows.Forms.BindingSource(this.components);
@@ -42,19 +43,32 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.patientGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.patientGrid.Location = new System.Drawing.Point(12, 31);
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.patientGrid.DefaultCellStyle = dataGridViewCellStyle1;
+            this.patientGrid.Location = new System.Drawing.Point(18, 14);
+            this.patientGrid.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.patientGrid.Name = "patientGrid";
-            this.patientGrid.Size = new System.Drawing.Size(769, 304);
+            this.patientGrid.RowTemplate.DefaultCellStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.patientGrid.Size = new System.Drawing.Size(1154, 502);
             this.patientGrid.TabIndex = 0;
             this.patientGrid.RowValidating += new System.Windows.Forms.DataGridViewCellCancelEventHandler(this.patientGrid_RowValidating);
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(793, 347);
+            this.ClientSize = new System.Drawing.Size(1190, 534);
             this.Controls.Add(this.patientGrid);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "MainForm";
             this.Text = "Напоминатель стоматолога";
             this.Load += new System.EventHandler(this.MainFormLoad);
